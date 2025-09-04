020b0884a564bd22de94e456b73ff1da5cd6b2e3
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-09-04 17:31:58 -0300

    feat: create select component

---
+++ web/src/components/Select.tsx
@@ -0,0 +1,15 @@
+
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {}
+
+export function Select({children, ...props}: SelectProps) {
+  return (
+    <select
+      name=""
+      id=""
+      className="w-full h-12 bg-white rounded-sm px-4 border-2 border-white border-solid outline-none shadow-[0px_4px_10px_rgba(0,0,0,0.04)] transition-colors ease-in focus:border-ultramarine-blue"
+      {...props}
+    >
+      {children}
+    </select>
+  )
+}
