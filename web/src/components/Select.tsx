6e1a79f1ef7350289bdc1ab00ddafc82540b5e94
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-09-10 18:02:51 -0300

    feat: improve ux in the mobile device

--- web/src/components/Select.tsx
+++ web/src/components/Select.tsx
@@ -6,7 +6,7 @@ export function Select({children, ...props}: SelectProps) {
     <select
       name=""
       id=""
-      className="w-full h-12 bg-white rounded-sm px-4 border-2 border-white border-solid outline-none shadow-[0px_4px_10px_rgba(0,0,0,0.04)] transition-colors ease-in focus:border-ultramarine-blue"
+      className="w-full h-12 bg-white rounded-sm px-4 border-2 border-white border-solid outline-none shadow-[0px_4px_10px_rgba(0,0,0,0.04)] transition-colors ease-in appearance-none focus:border-ultramarine-blue"
       {...props}
     >
       {children}
