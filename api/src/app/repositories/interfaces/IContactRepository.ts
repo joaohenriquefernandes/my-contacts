1f851b38759da878fbd73cc41ecac60a8f2a736d
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-09-12 16:23:22 -0300

    feat: create contact repository interface

---
+++ api/src/app/repositories/interfaces/IContactRepository.ts
@@ -0,0 +1,12 @@
+import type { CreateContactDTO } from '../dtos/CreateContactDTO.js';
+import type { UpdateContactDTO } from '../dtos/UpdateContactDTO.js';
+import type { Contact } from '../entities/Contact.js';
+
+export interface IContactRepository {
+  findAll(): Promise<Contact[] | null>;
+  findById(id: string): Promise<Contact | null>
+  findByEmail(email: string): Promise<Contact | null>
+  create(createContactDto: CreateContactDTO): Promise<Contact>
+  update(id: string, updateContactDto: UpdateContactDTO): Promise<Contact>
+  remove(id:  string): Promise<void>
+}
